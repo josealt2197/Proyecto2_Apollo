9923488012a3acfc9f84c1efb224c9d98d061a7e
JoseAlta <josealt2197@gmail.com>
2019-03-04 08:42:58 -0600

    Cargar Dropdowns Direccion

--- Proyecto2_Apollo/Models/UserModel.Context.cs
+++ Proyecto2_Apollo/Models/UserModel.Context.cs
@@ -28,5 +28,6 @@ namespace Proyecto2_Apollo.Models
         public virtual DbSet<Login> Logins { get; set; }
         public virtual DbSet<Question> Questions { get; set; }
         public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Place> Places { get; set; }
     }
 }
